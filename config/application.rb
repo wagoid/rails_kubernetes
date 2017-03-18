bd5f2824592e658cc979ebef4ebd6f95da4f553e
wagoid <w.rsantos@outlook.com>
2017-03-18 09:27:45 -0300

    Fix typo in deployment.yml

--- config/application.rb
+++ config/application.rb
@@ -27,9 +27,10 @@ module RailsKubernetes
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
 
-    config.session_store :cookie_store, key: '_rails_kubernetes_session'
-    config.middleware.use ActionDispatch::Cookies # Required for all session management
-    config.middleware.use ActionDispatch::Session::CookieStore, config.session_options
+    # Uncomment this (and also update initializers/devise_token_auth.rb) to enable login session saving
+    # config.session_store :cookie_store, key: '_rails_kubernetes_session'
+    # config.middleware.use ActionDispatch::Cookies # Required for all session management
+    # config.middleware.use ActionDispatch::Session::CookieStore, config.session_options
 
     config.middleware.insert_before 0, Rack::Cors do
       allow do
