2e745ad7136dbeb33853e592f537f57f1147079d
wagoid <w.rsantos@outlook.com>
2017-03-19 14:03:36 -0300

    Add CircleCI config

---
+++ config/circleci/deploy.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Exit on any error
+set -e
+
+sudo /opt/google-cloud-sdk/bin/gcloud docker -- push us.gcr.io/${PROJECT_NAME}/rku
+sudo chown -R ubuntu:ubuntu /home/ubuntu/.kube
+# Update deployment file with environment values
+envsubst < deployment.yml > deployment.parsed.yml
+# Applies the rolling update to the deployment
+kubectl patch -f deployment.yml -p "`cat deployment.parsed.yml`"
