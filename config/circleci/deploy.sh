aa76dfcf4cdfc47acef6b34efcbfff40cccc0721
wagoid <w.rsantos@outlook.com>
2017-03-19 14:55:29 -0300

    Update deploy config

--- config/circleci/deploy.sh
+++ config/circleci/deploy.sh
@@ -7,6 +7,6 @@ RAILS_ENV=production bundle exec rails db:migrate
 sudo /opt/google-cloud-sdk/bin/gcloud docker -- push us.gcr.io/${PROJECT_NAME}/rku
 sudo chown -R ubuntu:ubuntu /home/ubuntu/.kube
 # Update deployment file with environment values
-envsubst < deployment.yml > deployment.parsed.yml
+RAILS_ENV=production envsubst < deployment.yml > deployment.parsed.yml
 # Applies the rolling update to the deployment
 kubectl patch -f deployment.yml -p "`cat deployment.parsed.yml`"
